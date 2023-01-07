5f90f1bb36ae3fd620dcda002ae4b348f9200479
Eman <emmanuelgatwech@gmail.com>
2023-01-07 21:28:39 +0300

    chore: add prompts

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,5 +2,9 @@ module.exports = {
   reactStrictMode: true,
   experimental: {
     transpilePackages: ["ui"],
+  }
+  ,
+  images: {
+    domains: ["cloudinary.com/", "res.cloudinary.com"],
   },
 };
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Button } from "ui";
-import { Flex, Stack, Heading } from "@chakra-ui/react";
+import { Flex, Stack, Heading, Text } from "@chakra-ui/react";
 
 export default function Web() {
   return (
@@ -9,9 +9,28 @@ export default function Web() {
         <Heading size="md">
           Are you feeling happy, sad, or somewhere in between?
         </Heading>
+        <Text>By selecting the mood that best reflects your current emotional state, you'll help the app match you with other users who are feeling similarly.</Text>
+        <Flex gap={4}>
+          <Button>Excited</Button>
+          <Button>Argumentative</Button>
+          <Button>Curious</Button>
+          <Button>Doubtful</Button>
+          <Button>Cheerful</Button>
+        </Flex>
         <Heading size="md">
           What are you in the mood to talk about today?
         </Heading>
+        <Text>By selecting the mood that best reflects your current emotional state, you'll help the app match you with other users who are feeling similarly.</Text>
+        <Stack>
+          <Flex gap={4}>
+            <Button>Music</Button>
+            <Button>Fashion</Button>
+            <Button>Food</Button>
+            <Button>Sports</Button>
+            <Button>Games</Button>
+          </Flex>
+        </Stack>
+
         <Button>Start Quizzing!</Button>
       </Stack>
     </Flex>
